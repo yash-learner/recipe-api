927917672dbdcefa86ad69e7ff54a228dc286ba0
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-06-25 23:39:45 +0530

    Fix linting errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -14,7 +14,6 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
-
     def test_subtract_numbers(self):
         """Test subtracting two numbers"""
 
