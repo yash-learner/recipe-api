f93e5d2be9239df616bc8c182d94431402404547
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-06-25 23:25:05 +0530

    Fix linting errors

--- app/app/calc.py
+++ app/app/calc.py
@@ -8,7 +8,7 @@ def add(x, y):
     """Add x and y and return the result."""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtracr x from y and return the result."""
     return y - x
-
