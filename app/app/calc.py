6cb8b560808f4c0d6600e570d3f440b73a7bbaf9
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-06-25 23:11:50 +0530

    Configured Database in Django

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+
+"""
+Calculator tests
+"""
+
+
+def add(x, y):
+    """Add x and y and return the result."""
+    return x + y
+
+def subtract(x,y):
+    """Subtracr x from y and return the result."""
+    return y - x
+
---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""Sample tests for calc"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding two numbers."""
+
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+
+    def test_subtract_numbers(self):
+        """Test subtracting two numbers"""
+
+        res = calc.subtract(10,15)
+        self.assertEqual(res, 5)
+        
